5b14e81dbae2ce9d45c16cedb3aa9013872f73ec
albabernal03 <ALBABERO@MYUAX.COM>
2022-11-21 12:43:07 +0100

    hemos creado una funcion para ir creando el arbol de huffman

--- Tema4/ejercicio1.py
+++ Tema4/ejercicio1.py
@@ -21,7 +21,7 @@ def  ordenar_nodos(lista_nodos):
     lista_nodos= sorted(lista_nodos, key=lambda x: x.frecuencia)
     return lista_nodos  
 
-def insertar_nodo(lista_nodos, nodo):
+def insertar_nodo(lista_nodos, nodo): 
     for i in range(len(lista_nodos)):
         if nodo.frecuencia < lista_nodos[i].frecuencia:
             lista_nodos.insert(i, nodo)
@@ -30,11 +30,35 @@ def insertar_nodo(lista_nodos, nodo):
             break
     return lista_nodos
 
+def arbol_huffman(simbolos, frecuencias):
+    '''Funcion que crea un arbol de huffman a partir de una lista de simbolos y una lista de frecuencias'''
+    nodos=[]
+    for i in range(len(simbolos)):
+        nodos.append(nodoArbol(simbolos[i], frecuencias[i])) #con esto creamos una lista de nodos con los simbolos y frecuencias(desordenados)
+    nodos= ordenar_nodos(nodos) #ordenamos la lista de nodos
+    while len(nodos)>1: #mientras haya mas de un nodo en la lista
+        nodo= nodoArbol('XX', nodos[0].frecuencia + nodos[1].frecuencia) #creamos un nodo padre con la suma de las frecuencias de los dos primeros nodos
+        nodo.izquierda= nodos[0] #el primer nodo de la lista pasa a ser el hijo izquierdo del nodo padre
+        nodo.derecha= nodos[1] #el segundo nodo de la lista pasa a ser el hijo derecho del nodo padre
+        nodos[0].padre= nodo #el nodo padre pasa a ser el padre del primer nodo de la lista
+        nodos[1].padre= nodo #el nodo padre pasa a ser el padre del segundo nodo de la lista
+        nodos= nodos[2:] #eliminamos los dos primeros nodos de la lista
+        nodos= insertar_nodo(nodos, nodo) #insertamos el nodo padre en la lista de nodos
 
-#probamos la funcion ordenar_nodos
+    return nodos[0] #devolvemos el nodo raiz del arbol
 
-lista_nodos= [nodoArbol('I',0.28), nodoArbol('N',0.16), nodoArbol('T',0.08), nodoArbol('E',0.16), nodoArbol('L',0.08), nodoArbol('G',0.08), nodoArbol('C',0.08), nodoArbol('A',0.08)]
-lista_nodos= ordenar_nodos(lista_nodos)
-for nodo in lista_nodos:
-    print(nodo.simbolo, nodo.frecuencia)
+def buscar(raiz, clave):
+    '''Funcion que busca un simbolo en el arbol de huffman'''
+    pos= None
 
+    if raiz is not None:
+        if raiz.simbolo == clave:
+            pos= raiz
+            return pos
+        if pos is None:
+            pos= buscar(raiz.izquierda, clave)
+        if pos is None:
+            pos= buscar(raiz.derecha, clave)
+    return pos
+
+    
