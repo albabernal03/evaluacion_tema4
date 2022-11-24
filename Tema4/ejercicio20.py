4cf7b89d9d06d620d509d3d8619b8a620ae3f9a2
albabernal03 <ALBABERO@MYUAX.COM>
2022-11-24 19:06:19 +0100

    creamos el nodo del arbol

--- Tema4/ejercicio20.py
+++ Tema4/ejercicio20.py
@@ -6,17 +6,30 @@ with open('/Users/hectorbernaltrujillo/Documents/informática/Programación py
 
 #CREAMOS UNA LISTA CON LOS NOMBRES:
 nombres= []
-for i in range(1, len(lista)): 
-    nombres.append(lista[i][1])
+for i in range(1, len(lista)):  
+    nombres.append(lista[i][1]) #Cogemos la columna 1 que es la de los nombres
 
 #CREAMOS UNA LISTA CON LOS NUMEROS:
 numeros= []
 for i in range(1, len(lista)): 
-    numeros.append(lista[i][0])
+    numeros.append(lista[i][0]) #Cogemos la columna 0 que es la de los numeros
 
 #CREAMOS UNA LISTA CON LAS DEBILIDADES:
 debilidades= []
-for i in range(1, len(lista)):
-    debilidades.append(lista[i][2])
+for i in range(1, len(lista)): 
+    debilidades.append(lista[i][2]) #Cogemos la columna 2 que es la de las debilidades
 
+
+#CREAMOS EL ARBOL:
+
+class nodoArbol(object):
     
+    def __init__(self, info):
+        self.info = info
+        self.izq = None
+        self.der = None
+
+
+
+
+
