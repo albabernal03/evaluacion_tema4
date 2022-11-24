24dc32f870dc5c7bdd9cb802f931aeb416c1ccc0
albabernal03 <ALBABERO@MYUAX.COM>
2022-11-24 21:07:24 +0100

    cambios

--- Tema4/ejercicio2.py
+++ Tema4/ejercicio2.py
@@ -1,31 +1,29 @@
-#EN EL SIGUIENTE EJERCICIO SE NOS PIDE CREAR TRES ARBOLES, UNO CON LOS NOMBRE, NUMERO Y DEBILIDAD
-
-#Cogemos el csv y lo dividimos en una lista de listas
-import csv
+import csv 
 import numpy as np
-with open('/Users/hectorbernaltrujillo/Documents/informática/Programación python/ff/evaluacion_tema4/Tema4/pokemon.csv', 'r') as f:
+with open('/Users/hectorbernaltrujillo/Documents/informática/Programación python/ff/evaluacion_tema4/pokemon.csv', 'r') as f:
     reader = csv.reader(f)
     lista = list(reader)
 
-#Creamos una lista con los nombres de los pokemons
-nombres = []
-for i in range(1, len(lista)):
+#CREAMOS UNA LISTA CON LOS NOMBRES:
+nombres= []
+for i in range(1, len(lista)):  
     nombres.append(lista[i][1]) #Cogemos la columna 1 que es la de los nombres
 
-#Creamos una lista con los numeros de los pokemons
-numeros = []
-for i in range(1, len(lista)):
+#CREAMOS UNA LISTA CON LOS NUMEROS:
+numeros= []
+for i in range(1, len(lista)): 
     numeros.append(lista[i][0]) #Cogemos la columna 0 que es la de los numeros
 
-#Creamos una lista con los tipos de los pokemons
-tipos = []
-for i in range(1, len(lista)):
-    tipos.append(lista[i][2]) #Cogemos la columna 2 que es la de los tipos
+#CREAMOS UNA LISTA CON LAS DEBILIDADES:
+tipos= []
+for i in range(1, len(lista)): 
+    tipos.append(lista[i][2]) #Cogemos la columna 2 que es los tipos
 
-#Creamos tres arboles donde sus indices son el nombre, el numero y tipos
+
+#CREAMOS EL ARBOL:
 
 class nodoArbol(object):
-
+    
     def __init__(self, info):
         self.info = info
         self.izq = None
@@ -51,7 +49,6 @@ def buscar(raiz,clave):
             pos = buscar(raiz.der,clave)
     return pos
 
-
 def inorden(raiz): #Recorrido inorden: esta funcion nos debuelve una lista con los datos del arbol en orden de menor a mayor
     if raiz != None:
         inorden(raiz.izq)
@@ -62,7 +59,8 @@ def por_nivel(raiz):
     '''Funcion que nos imprime los datos del arbol por niveles'''
     cola = []
     cola.append(raiz)
-    while len(cola) > 0:
+
+    while len(cola) != 0:
         nodo = cola.pop(0)
         print(nodo.info)
         if nodo.izq != None:
@@ -70,98 +68,123 @@ def por_nivel(raiz):
         if nodo.der != None:
             cola.append(nodo.der)
 
-def mostrar_tipo(dato):
-    '''Funcion que muestra todos los tipos de pokemons y cantidad de cada uno'''
-    tipos = []
-    for i in range(1, len(lista)):
-        tipos.append(lista[i][3])
-    tipos = np.array(tipos)
-    tipos = np.unique(tipos)
-    for i in range(len(tipos)):
-        print(tipos[i], ':', list(tipos).count(tipos[i]))
-    
+def preorden(raiz): #Recorrido preorden: nos genera una replica del arbol
+    if raiz != None:
+        print(raiz.info)
+        preorden(raiz.izq)
+        preorden(raiz.der)
 
-#Creamos el arbol de nombres
 
+#APARATADO A:
+
+#creamos tres arboles, uno con los nombres, otro con los numeros y otro con las debilidades
+
+#NOMBRES:
 raiz = None
 for i in range(len(nombres)):
     raiz = insertar_nodo(raiz, nombres[i])
 
-#Creamos el arbol de numeros
-
+#NUMEROS:
 raiz2 = None
 for i in range(len(numeros)):
     raiz2 = insertar_nodo(raiz2, numeros[i])
 
-#Creamos el arbol de debilidades
-
+#TIPOS:
 raiz3 = None
 for i in range(len(tipos)):
     raiz3 = insertar_nodo(raiz3, tipos[i])
 
-#Mostramos los arboles
 
-print('Arbol de nombres')
-por_nivel(raiz)
-print('Arbol de numeros')
-por_nivel(raiz2)
-print('Arbol de debilidades')
-por_nivel(raiz3)
 
-#Mostramos los tipos de pokemon y su cantidad
 
-mostrar_tipo(lista)
+#APARTADO B:
 
-#Mostramos los pokemons que son de tipo agua
+#Buscamos el pokemon que queremos por su numero y nos devuelve su nombre y su tipo:
+def informacion_pokemon_numero(numero, numeros):
+    if numero in numeros:
+        posicion = numeros.index(numero)
+        nombre = nombres[posicion]
+        tipo = tipos[posicion]
+        print('El pokemon numero', numero, 'se llama', nombre, 'y es de tipo', tipo)
+    else:
+        print('El pokemon no existe')
 
-print('Pokemons de tipo agua')
-for i in range(1, len(lista)):
-    if lista[i][3] == 'Water':
-        print(lista[i][1])
+#Buscamos por proximidad el pokemon que queremos por su nombre, es decir si ponemos bul imprime todos con bul y nos devuelve su numero y su tipo:
 
-#Mostramos los pokemons que son de tipo fuego
+def proximidad_nombres(nombre, nombres):
+    for i in range(len(nombres)):
+        if nombre in nombres[i]:
+            posicion = nombres.index(nombres[i])
+            numero = numeros[posicion]
+            tipo = tipos[posicion]
+            print('El pokemon', nombres[i], 'tiene el numero', numero, 'y es de tipo', tipo)
 
-print('Pokemons de tipo fuego')
-for i in range(1, len(lista)):
-    if lista[i][3] == 'Fire':
-        print(lista[i][1])
 
-#Mostramos los pokemons que son de tipo planta
+#APARTADO C:
 
-print('Pokemons de tipo planta')
-for i in range(1, len(lista)):
-    if lista[i][3] == 'Grass':
-        print(lista[i][1])
+#Mostramos TODOS los pokemon que sean de un tipo determinado:
+#def mostrar_pokemon_tipo(tipo, tipos):
+ #TODO: hacer esta funciom
 
-#Mostramos los pokemons que son de tipo electrico
 
-print('Pokemons de tipo electrico')
-for i in range(1, len(lista)):
-    if lista[i][3] == 'Electric':
-        print(lista[i][1])
+#APARTADO D:
 
-#realizamos una busqueda en el arbol de nombres por proximidad
-
-print('Busqueda por proximidad')
-print('Introduce el nombre del pokemon')
-nombre = input()
-for i in range(len(nombres)):
-    if nombre in nombres[i]:
-        print(nombres[i])
-
-#realizamos un listado en orden ascedente por numero de pokemon
-
-print('Listado de pokemons por numero')
+#Realizar un listado en orden ascendente por numero
 inorden(raiz2)
 
-#realizamos un listado en orden ascedente por nombre de pokemon
-
-print('Listado de pokemons por nombre')
+#Realizar un listado en orden ascendente por nombre
 inorden(raiz)
 
-#realizamos un listado por nivel por nombre de pokemon
-
-print('Listado de pokemons por nivel')
+#Listado por nivel por nombre
 por_nivel(raiz)
 
-#
+
+
+#APARTADO E:
+
+#Mostra todos los pokemons que son debiles frente a Jolteon(es decir, que tengan Electric en su lista de debilidades), Lycanroc (es decir, que tengan Fighting en su lista de debilidades) y Tyranrum (es decir, que tengan Dragon en su lista de debilidades).
+
+def debil(Jolteon, Lycanroc, Tyrantrum):
+    for i in range(len(tipos)):
+        if 'Electric' in tipos[i]:
+            posicion = tipos.index(tipos[i])
+            nombre = nombres[posicion]
+            numero = numeros[posicion]
+            print('El pokemon', nombre, 'tiene el numero', numero, 'y es debil frente a Jolteon')
+        if 'Fighting' in tipos[i]:
+            posicion = tipos.index(tipos[i])
+            nombre = nombres[posicion]
+            numero = numeros[posicion]
+            print('El pokemon', nombre, 'tiene el numero', numero, 'y es debil frente a Lycanroc')
+        if 'Dragon' in tipos[i]:
+            posicion = tipos.index(tipos[i])
+            nombre = nombres[posicion]
+            numero = numeros[posicion]
+            print('El pokemon', nombre, 'tiene el numero', numero, 'y es debil frente a Tyrantrum')
+
+#APARTADO F:
+
+#mostrar todos los pokemons hay en el arbol
+
+def mostrar_pokemons(raiz):
+    if raiz != None:
+        mostrar_pokemons(raiz.izq)
+        print(raiz.info)
+        mostrar_pokemons(raiz.der)
+
+#mostramos la cantidad de pokemons que hay en el arbol
+def valores_unicos(raiz3, unicos):
+    '''Funcion que nos devuelve la cantidad de valores unicos que hay en el arbol'''
+    if raiz3 is not None:
+        if raiz3.info not in unicos:
+            unicos[raiz3.info] = 1
+        else:
+            unicos[raiz3.info] = unicos[raiz3.info] + 1
+        unicos = valores_unicos(raiz3.izq, unicos) #recorremos el arbol por la izquierda
+        unicos = valores_unicos(raiz3.der, unicos) #recorremos el arbol por la derecha
+    return unicos
+
+#probamos la funcion
+unicos = {}
+unicos = valores_unicos(raiz3, unicos)
+print(unicos)
--- Tema4/menu.py
+++ Tema4/menu.py
@@ -1,1 +1,61 @@
 from ejercicio1 import *
+from ejercicio2 import *
+import time # Importamos la libreria time para poder usar la funcion sleep 
+
+
+def iniciar():
+
+    while True:
+
+        print('----------------------------------------')
+        print('-----------------MENU-------------------')
+        print('----------------------------------------')
+        print('[1] Ejercicio 1: Codificar y decodificar')
+        print('[2] Ejercicio 2: Pokemons')
+        print('[3] Ejercicio 3: Las 7 Maravillas')
+        print('[4] Ejercicio 4: salir del programa')
+        print('----------------------------------------')
+
+        opcion = input('Introduce una opcion: ')
+
+        if opcion == '1':
+            print('.....Cargando.....').sleep(2)
+            mensaje= input('Introduce el mensaje a codificar: ')
+            simbolos= ['A', 'F', '1', '3', 'O', 'M', 'T']
+            frecuencias= [0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15]
+            raiz= arbol_huffman(simbolos, frecuencias)
+            codigo= codificar(mensaje, raiz)
+            print(codigo)
+            mensaje= decodificar(codigo, raiz)
+            print(mensaje)
+
+        elif opcion == '2':
+            print('.....Cargando.....').sleep(2)
+            while  True:
+                print('[1] Buscar pokemos por nombre o por proximidad')
+                print('[2] Buscar pokemons por tipo (Fuego, Agua, Planta y Eléctrico)')
+                print('[3] Mostrar listado por nombre ')
+                print('[4] Mostrar listado por número')
+                print('[5] Mostrar los Pokemos que son debiles a (Jolteon, Lycanroc y Tyrantrum)')
+                print('[6] Mostrar todos los tipos de pokemos y la cantidad de pokemos que hay de cada tipo')
+                print('[7] Listado de nombres por nivel')
+                print('[8] Nada')
+
+                opcion = input('Introduce una opcion: ')
+
+                if opcion == '1':
+                    print('.....Cargando.....').sleep(2)
+                    nombre = input('Introduce el nombre del pokemon: ')
+                    proximidad_nombres(nombre)
+
+
+iniciar()
+                    
+
+
+
+
+
+
+
+
