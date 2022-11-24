188f8b01ce78c8304ba4f8c8dedabae9c234ad4e
albabernal03 <ALBABERO@MYUAX.COM>
2022-11-24 18:57:21 +0100

    creamos una lista con los nombres

--- Tema4/ejercicio20.py
+++ Tema4/ejercicio20.py
@@ -1,1 +1,10 @@
 import csv 
+
+with open('/Users/hectorbernaltrujillo/Documents/informática/Programación python/ff/evaluacion_tema4/Tema4/pokemon.csv', 'r') as f:
+    reader = csv.reader(f)
+    lista = list(reader)
+
+#CREAMOS UNA LISTA CON LOS NOMBRES:
+nombres= []
+for i in range(1, len(lista)): 
+    nombres.append(lista[i][1])
